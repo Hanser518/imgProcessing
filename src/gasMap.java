f230e4d3e8ed197346fb7b1c86d5508817dfa3d1
Sanfu <2642830884@qq.com>
2024-09-01 23:56:08 +0800

    ultraGas引入gasMap机制

---
+++ src/gasMap.java
@@ -0,0 +1,22 @@
+import Controller.ImgProcessingController;
+import Entity.IMAGE;
+import Service.ICalculateService;
+import Service.Impl.ICalculateServiceImpl;
+
+import java.io.IOException;
+
+public class gasMap {
+    static ICalculateService calculateServer = new ICalculateServiceImpl();
+    static ImgProcessingController imgCtrl = new ImgProcessingController();
+    public static void main(String[] args) throws IOException {
+        IMAGE px = new IMAGE("index3.png");
+        int[][] map = calculateServer.getGasMap(px, 30, 40);
+        for(int i = 0;i < map.length;i ++){
+            for(int j = 0;j < map[i].length; j++){
+                System.out.printf("%2d ", map[i][j]);
+            }
+            System.out.printf("\n");
+        }
+
+    }
+}
