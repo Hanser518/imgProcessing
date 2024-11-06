9dbc5742e9ee113e7eb150f812a24445443993f9
liang <sanfu518@qq.com>
2024-11-06 14:43:31 +0800

    ？

---
+++ src/algorithm/wpfo/main/WPFO2.java
@@ -0,0 +1,28 @@
+package algorithm.wpfo.main;
+
+import algorithm.edgeTrace.entity.Node;
+import algorithm.edgeTrace.main.EdgeTrace;
+import entity.IMAGE;
+
+public class WPFO2 implements Runnable{
+
+    private static EdgeTrace edgeTraceService;
+    private static Integer focusCount = 0;
+
+    public WPFO2(IMAGE px){
+        edgeTraceService = new EdgeTrace(px);
+
+
+    }
+
+    @Override
+    public void run() {
+        // 获取
+        edgeTraceService.start(EdgeTrace.PATTERN_ONE);
+        for (Node node : edgeTraceService.getPathList()){
+            if(node.getNodeSize() > 8){
+
+            }
+        }
+    }
+}
