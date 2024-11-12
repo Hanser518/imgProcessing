846d9204c70f362ed88bc5892dbbd3cfa06b3f96
liang <sanfu518@qq.com>
2024-11-12 17:58:09 +0800

    开坑

--- src/algorithm/cnn/service/ConvCalcService.java
+++ src/algorithm/cnn/service/ConvCalcService.java
@@ -5,25 +5,40 @@ import algorithm.cnn.entity.ImagePool;
 import entity.IMAGE;
 import service.imgService;
 import service.impl.imgServiceImpl;
+import threadPool.core.ThreadPoolReflectCore;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class ConvCalcService {
-    static private final imgService imgServ = new imgServiceImpl();
+    public static enum FEATURE {EASY, NORMAL, PRECISION}
 
-    public ImagePool edgeCalc(ImagePool pool){
+    private static final imgService imgServ = new imgServiceImpl();
+    private static ThreadPoolReflectCore conv2;
+
+    public ImagePool convCalc(ImagePool pool, FEATURE... model){
+        FEATURE processModel;
+        if(model.length < 1){
+            processModel = FEATURE.NORMAL;
+        }else{
+            processModel = model[0];
+        }
+        return null;
+    }
+
+
+    @Deprecated
+    public ImagePool edgeCalc(ImagePool pool) {
         List<ImageCNN> imgList = pool.getImageList();
         List<ImageCNN> result = new ArrayList<>();
-        for(ImageCNN img : imgList){
+        for (ImageCNN img : imgList) {
             try {
                 int[][] res = imgServ.getSobelEdge(new IMAGE(img.getArgbMatrix()));
                 result.add(new ImageCNN(res));
-            }catch (Exception ignored){
+            } catch (Exception ignored) {
             }
         }
         pool.setImageList(result);
         return pool;
     }
-
 }
